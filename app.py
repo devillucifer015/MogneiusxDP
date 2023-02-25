8d3ee9cb24771f667367394ce41570c21e056e73
devillucifer015 <123466455+devillucifer015@users.noreply.github.com>
2023-02-25 12:16:33 +0530

    Create app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://devillucifer015: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@github.com/devillucifer015/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://devillucifer015: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@github.com/devillucifer015/xDEADPOOLAUTOFILTERv1 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
